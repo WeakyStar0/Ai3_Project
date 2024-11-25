93587eed92b401dd97e0ffadd65ef24d5d98e8f0
Tiriri420 <tomas2002pereira@gmail.com>
2024-11-25 10:51:55 +0000

    organização de prioridade (Foi o stor)

--- server/routes/reservations.routes.js
+++ server/routes/reservations.routes.js
@@ -2,10 +2,13 @@ const express = require("express");
 const router = express.Router();
 const reservationsController = require("../controllers/reservations.controller");
 
-router.get("/", reservationsController.listReservations);
-router.post("/", reservationsController.createReservation);
 router.get("/:reservationId", reservationsController.getReservation);
+router.get("/", reservationsController.listReservations);
+
+router.post("/", reservationsController.createReservation);
+
 router.put("/:reservationId", reservationsController.updateReservation);
+
 router.delete("/:reservationId", reservationsController.cancelReservation);
 
 module.exports = router;
--- server/routes/rooms.routes.js
+++ server/routes/rooms.routes.js
@@ -2,10 +2,13 @@ const express = require("express");
 const router = express.Router();
 const roomsController = require("../controllers/rooms.controller");
 
-router.get("/", roomsController.listRooms);
-router.post("/", roomsController.createRoom);
 router.get("/:roomId", roomsController.getRoomDetails);
+router.get("/", roomsController.listRooms);
+
+router.post("/", roomsController.createRoom);
+
 router.put("/:roomId", roomsController.updateRoom);
+
 router.delete("/:roomId", roomsController.deleteRoom);
 
 module.exports = router;
